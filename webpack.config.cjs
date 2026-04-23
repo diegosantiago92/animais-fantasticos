391fc7cb80ae0ab12c8270b4a1dd5fef8fbfb04e
Diego Santiago <diegosantiago92@outlook.com>
2026-04-22 22:52:09 -0300

    feat: adiciona o plugin HtmlWebpackPlugin ao projeto para otimizar a geração de arquivos HTML

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./js/script.js",
@@ -21,4 +22,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
